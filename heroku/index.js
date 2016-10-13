8b7e0aac4a27bc7c443cf9b67cd9d1740ce983cb
aaronbaik90 <doosan.baik.90@gmail.com>
2016-10-13 00:31:05 -0400

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -25,7 +25,7 @@ io.on('connection', function (socket) {
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) { res.sendFile('index.html'); });
+app.get('/', function(req, res) { res.sendFile('index.html', { root: __dirname} ); });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
