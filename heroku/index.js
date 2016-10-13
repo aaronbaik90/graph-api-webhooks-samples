fa351dea2c6fbfede9f739267ddaebff72cfef49
aaronbaik90 <doosan.baik.90@gmail.com>
2016-10-13 02:07:24 -0400

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -10,20 +10,23 @@ var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/'));
 var xhub = require('express-x-hub');
-var socketIO = require('socket.io');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-var server = require('http').Server(app);
-// setup socket io for communication
-const io = socketIO(server);
-io.on('connection', function (socket) {
-  console.log('Client connected');
-  socket.on('disconnect', function() { console.log('Client disconnected'); });
-}); 
+// websocket setup
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var server = http.createServer(app);
+server.listen(port);
+var wss = new WebSocketServer({server: server});
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
-app.use(bodyParser.json());
+wss.on("connection", function(ws) {
+  console.log("Web Socket Connected");
+  ws.on("close", function() {
+    console.log("Web Socket Closed");
+  }
+});
+
 
 app.get('/', function(req, res) { res.sendFile('index.html', { root: __dirname} ); });
 
@@ -40,28 +43,13 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(io, req, res) {
   console.log('Facebook request body:');
-
-  /*
-  if (req.isXHub) {
-    console.log('request header X-Hub-Signature found, validating');
-    if (req.isXHubValid()) {
-      console.log('request header X-Hub-Signature validated');
-      res.send('Verified!\n');
-    }
-  }
-  else {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.send('Failed to verify!\n');
-    // recommend sending 401 status in production for non-validated signatures
-    // res.sendStatus(401);
-  } */
   console.log(req.body);
   var entry = req.body.entry;
   for (i = 0 ; i < entry.length; i++) {
     var changes = entry[i].changes;
     for (j = 0; j < changes.length; j++) {
       console.log(changes[j]);
-      io.emit('update_feed', {message: 'Received Message'} );
+      wss.send(JSON.stringify({update_feed: "message received!"}));
     }
   }
   
