1f3113e1eaf265a02bae5f981ac67c6cd704f178
aaronbaik90 <doosan.baik.90@gmail.com>
2016-10-12 22:27:49 -0400

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('It works!');
+  res.sendfile('index.php', {root: __dirname });
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
