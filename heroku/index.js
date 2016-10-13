6d60aa4cd24ad92a372b747068be1e8692c1e8e9
aaronbaik90 <doosan.baik.90@gmail.com>
2016-10-13 02:21:01 -0400

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -7,7 +7,7 @@
  */
 var express = require('express');
 var app = express();
-var port =  (process.env.PORT || 5000));
+var port =  (process.env.PORT || 5000);
 app.use('/', express.static(__dirname + '/'));
 
 // websocket setup
